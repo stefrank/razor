9219c72a81e7a36280afb53077e3b22b75bf3a82
stefrank <stefq1@163.com>
2017-12-22 13:05:13 +0800

    some change

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Braintree;
@@ -21,7 +17,7 @@ namespace WebAPI.Controllers
             {
                 Amount = 1000.0M,
                 MerchantAccountId = "USD",
-                PaymentMethodNonce = Request.Form["payment_method_nonce"],
+                PaymentMethodNonce = collection["payment_method_nonce"],
                 ShippingAddress = new AddressRequest
                 {
                     FirstName = "Jen",
@@ -55,8 +51,41 @@ namespace WebAPI.Controllers
             }
             else
             {
-                Response.WriteAsync(result.Message);
+                Response.WriteAsync("Error Message:" + result.Message);
             }
         }
+
+        // // GET api/values
+        // [HttpGet]
+        // public IEnumerable<string> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
+
+        // // GET api/values/5
+        // [HttpGet("{id}")]
+        // public string Get([FromQuery]int id)
+        // {
+        //     return "value";
+        // }
+
+        // // POST api/values
+        // [HttpPost]
+        // public void Post([FromBody]string value)
+        // {
+        // }
+
+        // // PUT api/values/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody]string value)
+        // {
+        // }
+
+        // // DELETE api/values/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
+
     }
 }
--- Pages/ECBT/checkout.cs
+++ Pages/ECBT/checkout.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Braintree;
-using Microsoft.AspNetCore.Http;
 
 namespace razor.Pages
 {
